4ac7db24c387415ba7fa13e537f8de0d4e76322d
Sayan Bhattacharya <sayanbhattacharya06@gmail.com>
2018-06-03 23:25:00 +0530

    Initial commit

---
+++ app/src/main/java/in/encryptsolutions/slider/MainActivity.java
@@ -0,0 +1,18 @@
+package in.encryptsolutions.slider;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private SlideAdapter myAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        myAdapter = new SlideAdapter(this);
+        viewPager.setAdapter(myAdapter);
+    }
+}
